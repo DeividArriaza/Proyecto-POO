d46037ad858f1bbe59c7ed6967a37327562b3671
HipWilson <pen24760@uvg.edu.gt>
2024-09-19 17:47:03 -0600

    Update Vehiculo.java
    Modificacion clase vehiculo

---
+++ Vehiculo.java
@@ -0,0 +1,29 @@
+public class Vehiculo {
+    private String tipoVehiculo;
+
+    public Vehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    // Método para obtener la velocidad promedio
+    public double velocidadPromedio() {
+        switch (tipoVehiculo) {
+            case "Carro":
+                return 80.0; // ejemplo de velocidad promedio en km/h
+            case "Moto":
+                return 50.0;
+            case "Bicicleta":
+                return 20.0;
+            default:
+                return 0.0; // Si el tipo de vehículo no está definido
+        }
+    }
+}
