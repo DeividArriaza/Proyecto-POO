6863de08430fc7316638bc650892d37189baabd2
juanfriorozco8 <oro24647@uvg.edu.gt>
2024-09-19 18:01:15 -0600

    Gets y Sets de AdministracionRutas

--- AdministracionRutas.java
+++ AdministracionRutas.java
@@ -3,6 +3,40 @@ public class AdministracionRutas {
     private Vehiculo vehiculo;
     private Usuario usuario;
     private String horaInicio;
+
+
+    public Ruta getRuta() {
+        return this.ruta;
+    }
+
+    public void setRuta(Ruta ruta) {
+        this.ruta = ruta;
+    }
+
+    public Vehiculo getVehiculo() {
+        return this.vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Usuario getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getHoraInicio() {
+        return this.horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
     
 
 }
