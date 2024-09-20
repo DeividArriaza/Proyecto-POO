8fb4685bd2b292b006a7058bb8a81057c73a56cd
DeividArriaza <lop24730@uvg.edu.gt>
2024-09-20 04:04:07 -0600

    Update Main.java
    Iniciador de menu

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+		int opcion = 0;
+		
         Scanner scanner = new Scanner(System.in);
         System.out.println(" --- Bienvenido --- ");
-
-
+		System.out.println("¿Tienes una cuenta con nosotros?");
+		System.out.println("1. Si);
+		System.out.println("2. No); 
+		
+		while(opcion!=
+		
     }
     
 }
