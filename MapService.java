a1b0f758d66caa1a84b3d8fa952b2bff3a538924
juanfriorozco8 <oro24647@uvg.edu.gt>
2024-10-16 23:42:30 -0600

    librerias a usar

---
+++ MapService.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URLEncoder; //codificador de URL
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.time.Duration;
+public class MapService {
+    
+}
